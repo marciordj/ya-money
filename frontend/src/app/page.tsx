cbc725eeae258a325589acb726e2f78c4cbcb0b6
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-04-11 23:44:57 -0300

    add table example

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,68 @@
-import { Button } from "@/components/ui/button";
+import { 
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
+
+const data: Payment[] = [
+  {
+    id: "m5gr84i9",
+    amount: 316,
+    status: "success",
+    email: "ken99@yahoo.com",
+  },
+  {
+    id: "3u1reuv4",
+    amount: 242,
+    status: "success",
+    email: "Abe45@gmail.com",
+  },
+  {
+    id: "derv1ws0",
+    amount: 837,
+    status: "processing",
+    email: "Monserrat44@gmail.com",
+  },
+  {
+    id: "5kma53ae",
+    amount: 874,
+    status: "success",
+    email: "Silas22@gmail.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
+]
+ 
+export type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
 
 export default function Home() {
+  
   return (
-  <h1>Hello</h1>
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader className="text-slate-100">
+          123
+        </TableHeader>
+        <TableBody>
+        <TableRow>
+              <TableCell className="h-24 text-center text-slate-100">
+                No results.
+              </TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   );
 }
